56dc82e703fe57ed900ae6f18b05281396949549
Sean 'Blitz' Homer <sean.homer@therefore.ca>
2015-07-28 14:42:41 -0400

    - Corrections to the sass compile and build process, ensuring the right files are served for dev or dist as needed, with the correct options and file names.

--- templates/common/gulp/sass.js
+++ templates/common/gulp/sass.js
@@ -2,46 +2,44 @@
 
 import gulp from 'gulp';
 import autoprefixer from 'gulp-autoprefixer';
-import concat from 'gulp-concat';<% if(cssProcessor === 'libSass') { %>
-import sass from 'gulp-sass';<% } %><% if(cssProcessor === 'compass') { %>
-import compass from 'gulp-compass';<% } %>
+import rename from 'gulp-rename';
+import concat from 'gulp-concat';
+import compass from 'gulp-compass';
 import sourcemaps from 'gulp-sourcemaps';
 import minifyCss from 'gulp-minify-css';
 import gulpkss from 'gulp-kss';
 import gulpif from 'gulp-if';
 
-<% if(cssProcessor === 'compass') { %>
 let compassOptions = {
   //config_file: './config.rb',
   sass: global.paths.src,
   css: global.paths.tmp + 'css',
-  require: ['susy', 'modular-scale', 'breakpoint','font-awesome-sass']
-};<% } %>
-
+  require: ['susy', 'modular-scale', 'breakpoint', 'font-awesome-sass']
+};
+let compSassFilename = global.comp.name + '.css';
 /**
  * Sass Tasks
  */
-let sassCompilation = function(taskName, destDir, browserSync, minify = false, kss = false) {
-  // Compile SASS with sourcemaps + livereload.
-  gulp.task(taskName,['lintsass'], function () {
-    gulp.src(global.paths.fonts)
-      .pipe(gulp.dest(destDir + 'fonts'));
+let sassCompilation = function (taskName, browserSync, dist = false) {
+  let dest = dist ? global.paths.dist : global.paths.tmp;
 
-    gulp.src(global.paths.sass)<% if(cssProcessor === 'libSass') { %>
-      .pipe(sass({
-        errLogToConsole: true,
-        outputStyle: 'expanded'
-      }).on('error', sass.logError))<% } %><% if(cssProcessor === 'compass') { %>
-      .pipe(compass(compassOptions))<% } %>
-      .pipe(concat('hence-comp-ui-sample.css'))
+  // Compile SASS with sourcemaps + livereload.
+  gulp.task(taskName, ['lintsass'], function () {
+    gulp.src(global.paths.fonts)
+      .pipe(gulp.dest(dest + 'fonts'));
+
+    gulp.src(global.paths.sass)
+      .pipe(compass(compassOptions))
+      .pipe(concat(compSassFilename))
       .pipe(autoprefixer())
       .pipe(sourcemaps.init({loadMaps: true})) // loads map
-      .pipe(gulpif(minify,minifyCss()))
+      .pipe(gulpif(dist, minifyCss()))
+      .pipe(gulpif(dist, rename({suffix: '.min'})))
       .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest(destDir + 'css'))
+      .pipe(gulp.dest(dest + 'css'))
       .pipe(browserSync.stream());
 
-    if(kss) {
+    if (dist) {
       gulp.src(global.paths.sass)
         .pipe(gulpkss({
           overview: global.paths.dist + 'css/styleguide.md'
