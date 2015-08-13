1748cc78915175875bbb8b1b454e4c0ba1e492bb
Sean Homer <sean.homer@therefore.ca>
2015-08-13 16:26:11 -0400

    - massive renaming, removing 'hence-comp...' from all current components and repos in favor of short specific names

--- generators/component/template/common/gulpfile.babel.js
+++ generators/component/template/common/gulpfile.babel.js
@@ -1,58 +1,6 @@
 import gulp from 'gulp';
 import requireDir from 'require-dir';
 
-var srcDir = './src/';
-var tmpDir = './.tmp/';
-var buildSrcDir = './gulp/src/';
-
-global.comp = {
-  name: '<%= compName %>',
-  js: '<%= compName %>.js',
-  scss: '<%= compName %>.scss',
-  css: '<%= compName %>.css',
-  html: '<%= compName %>.html',
-  camel: '<%= compClassName %>'
-};
-
-// Specify paths & globbing patterns for tasks.
-global.paths = {
-  // HTML sources.
-  'html': srcDir + '**/*.html',
-  // Bower
-  'bower': './bower_components/',
-  // Build Src Dir
-  buildSrcDir: buildSrcDir,
-  // Dist JS file
-  'distjs': buildSrcDir + 'dist.js',
-  // Dev JS file
-  'devjs': srcDir + 'index.js',
-  // JS sources.
-  'gulp': srcDir + 'gulp/**/*.js',
-  // JS sources.
-  'js': srcDir + '**/*.js',
-  // SASS sources.
-  'sass': srcDir + '**/*.scss',
-  // Fonts
-  'fonts': './fonts/**',
-  // Image sources.
-  'img': srcDir + 'img/*',
-  // Sources folder.
-  'src': srcDir,
-  // Compiled CSS folder.
-  'css': srcDir + 'css',
-  // Distribution folder.
-  'dist': './dist/',
-  // Temp folder.
-  'tmp': tmpDir,
-  // Output css filepath
-  tmpCssOutput: `${tmpDir}css/`,
-  tmpCssOutputFile: `${tmpDir}css/${global.comp.name}.css`,
-  // WCT Test folder
-  'testBehaviour': './test/behaviour/**/*',
-  // Unit Test folder
-  'testUnit': './test/unit/**/*'
-};
-
 // Require all tasks in the 'gulp' folder.
 requireDir('./gulp', {recurse: false});
 requireDir('./gulp/build', {recurse: false});
