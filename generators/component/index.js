6edaaf40cc2623aeb671a357e695321f8d1e2697
Sean 'Blitz' Homer <sean.homer@therefore.ca>
2015-08-25 15:56:21 -0400

    - Bringing the scaffolding documentation up to par with all of the improvements and revisions applied to glush-util - Adding details on how to leverage a multi-install instance of the scaffold, and make use of cli flags with the normal start

--- generators/component/index.js
+++ generators/component/index.js
@@ -127,28 +127,29 @@ module.exports = function (done) {
     compPrefix: glush.env.pre || 'hence'
   };
 
+  // Because glush leverages gulp-util, the .env for cli args is available
+  // We must always drop the first non-flagged arg, as it's always your generator's name
+  var cliArgs = _.drop(glush.env._);
+
   // The first _ arg is always the generator name, so only run the multi-install on 2 args passed or more
   // This don't count actual flags set '--falg', just normal args on the cli
-  if (glush.env._.length > 1) {
-    var installOptions = _.chain(glush.env._)
-      .drop() // drop the first arg (generator name)
-      .map(function (arg) {
-        var splitName = arg.split(':');
+  if (cliArgs.length) {
+    var installOptions = _.map(cliArgs, function (arg) {
+      var splitName = arg.split(':');
 
-        return {
-          content: {
-            intro: glush.ascii.heading('Component Installation') +
-            glush.colors.bold(' Name: ') + cli.compPrefix + '-' + splitName[0] +
-            glush.colors.bold('\n Type: ') + splitName[1],
-            done: glush.ascii.spacer()
-          },
-          defaults: _.extend({
-            compName: splitName[0],
-            compType: splitName[1]
-          }, cli)
-        };
-      })
-      .value();
+      return {
+        content: {
+          intro: glush.ascii.heading('Component Installation') +
+          glush.colors.bold(' Name: ') + cli.compPrefix + '-' + splitName[0] +
+          glush.colors.bold('\n Type: ') + splitName[1],
+          done: glush.ascii.spacer()
+        },
+        defaults: _.extend({
+          compName: splitName[0],
+          compType: splitName[1]
+        }, cli)
+      };
+    });
 
     //console.log('installers', installOptions, glush.env);
     //scaffold._debug = true;
