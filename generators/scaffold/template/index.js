8f85c42d1a76356a303ab498300f48f50647f6bb
Sean 'Blitz' Homer <sean.homer@therefore.ca>
2015-09-01 15:48:19 -0400

    - improving and documenting the scaffold sub-generator

--- generators/scaffold/template/index.js
+++ generators/scaffold/template/index.js
@@ -1,20 +1,21 @@
-// Libraries
-var _ = require('lodash');
-var path = require('path');
-
-// Utils
-var inquisitor = require('hence-inquisitor');
-var hence = require('hence-util');
+// Essential Supporting Libraries/Utils
+var _ = require('lodash'); // The epic JS multi-tool!
+var path = require('path'); // For safe path joining
+var inquisitor = require('hence-inquisitor'); // The star of the show!
+var hence = require('hence-util'); // Supporting text tools
 
 // Paths
-var cwd = process.cwd();
-var tplDir = __dirname + '/template/';
+var cwd = process.cwd(); // The CWD of where the CLI is executed from, to reference where we'd like to output things
+var tplDir = __dirname + '/template/'; // The local reference to this scaffold template folder, to be used in installation
 
-
+// Start building out the scaffold installation
 var scaffold = inquisitor.Scaffold({
+  // Define the steps that your scaffold will use, in any order you desire. Each steps will receive answers from the
+  // former step, so it's best to place over arching steps that need previous answers to be further down the pipe.
   steps: [
     require('./scaffold/step-install-options')
   ],
+  // Specific the default set of answers options that will feed details into all of your steps, common config like paths.
   defaults: {
     dirs: {
       template: {
@@ -25,6 +26,9 @@ var scaffold = inquisitor.Scaffold({
       dest: path.join(cwd, 'generators')
     }
   },
+  // Control what messages appear during your installation. The scaffold manages the initial introduction, as well
+  // as completion message once everything is done. Should your process be aborted or encounter an error, you can
+  // manage what details go along with this as well.
   content: {
     intro: hence.ascii.hence(
       inquisitor.colors.bold(" Welcome to the Hence.io Scaffolding Sub-generator. ") + "This installer is designed to" +
@@ -34,6 +38,8 @@ var scaffold = inquisitor.Scaffold({
     " Review the possible gulp commands available to you on the project documentation, or type '" +
     inquisitor.colors.bold('gulp help') + "' at any time."
   },
+  // A helper function to configure CLI arguments passed into this installer, allowing you to process each argument
+  // and determine what unique options it will leverage during an multi-installation
   cliArg: function (arg) {
     return {
       content: {
@@ -46,6 +52,8 @@ var scaffold = inquisitor.Scaffold({
       }
     };
   },
+  // Provide a reference to the essential install function, which controls what you do with all of the answers once
+  // the steps are complete.
   install: require('./scaffold/install')
 });
 
