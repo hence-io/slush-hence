cfce78bbba1e59f6234d41a657a476224f29a7de
Sean 'Blitz' Homer <sean.homer@therefore.ca>
2015-09-28 11:12:21 -0400

    - including the updated sudo detection for gem installation on the install-deps action, checking whether or not windows is in use or osx/linux

--- generators/component/template/common/gulp/install.js
+++ generators/component/template/common/gulp/install.js
@@ -1,11 +1,19 @@
 import gulp from 'gulp';
 import {execSync} from 'child_process';
 
-gulp.task('install-deps', function (done) {
+gulp.task('install-deps', ['install-gems'], function (done) {
   //execSync('npm i -g karma-cli', {stdio: 'inherit'});
   //execSync('npm i', {stdio: 'inherit'});
   execSync('bower i', {stdio: 'inherit'});
-  execSync(<% if(cssProcessor === 'libSass') { %>"gem update && gem install scss_lint"<% } %><% if(cssProcessor === 'compass') { %>"gem update && gem install compass susy modular-scale breakpoint scss_lint font-awesome-sass"<% } %>, {stdio: 'inherit'});
+
+  done();
+});
+
+gulp.task('install-gems', function (done) {
+  let isWin = /^win/.test(process.platform);
+  let sudo = isWin ? '' : 'sudo ';
+
+  execSync(<% if(cssProcessor === 'libSass') { %>`${sudo} gem update && gem install scss_lint`<% } %><% if(cssProcessor === 'compass') { %>`${sudo} gem update && ${sudo} gem install compass susy modular-scale breakpoint scss_lint font-awesome-sass`<% } %>, {stdio: 'inherit'});
 
   done();
 });
